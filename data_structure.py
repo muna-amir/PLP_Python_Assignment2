89408e01108896d6287a675a38091eb253499618
muna-amir <117766211+muna-amir@users.noreply.github.com>
2024-03-03 20:31:35 +0300

    Create data_structure.py

---
+++ data_structure.py
@@ -0,0 +1,12 @@
+my_list = []
+list_b = [50, 60, 70]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1,15)
+my_list.extend(list_b)
+my_list.pop(-1)
+my_list.sort()
+print(my_list.index(30))
+print(my_list)
